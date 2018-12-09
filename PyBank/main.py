fe1eb8b6fa5c1f1feed3c9d97239b6e8bd3f8412
The Hoff <11578521+xjarodx@users.noreply.github.com>
2018-12-09 00:17:45 -0500

    Having a hard time getting the sum of the second column.

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,28 +3,45 @@ import csv
 
 budget_csv = os.path.join("../PyBank", "budget_data.csv")
 
-#def get_months():
-#    months=len(list(reader -1))
-
-
-dict_list = []
-
-
 with open(budget_csv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+    #skip header row
+    next(csvreader, None) 
     
-    #reader = csv.DictReader(open(budget_csv, 'rb'))
+    # Gets number of rows in csv
+    data=list(csvreader)
     
-    for line in csvreader:
-        dict_list.append(line)
+    row_count = len(data)
+
+    #Sets initial value of the net sum
+    net = 0
+
+    #gets values from rows
+    for row in csvreader:
+        net += float(row[1])
 
 
 
+
+
+print ("Financial Analysis")
+print ("-------------------------------")
+print ("Total Months: ", row_count)
+print ("Total: {}".format(net))
+print ("Average Change: ")
+print ("Greatest Increase in Profits: ")
+print ("Greatest Decrease in Profits: ")
 
 
 
 output_file = os.path.join("Analysis.txt")
 
 #  Open the output file
-with open(output_file, "w", newline="") as textfile:
-    writer = textfile.writer(dict_list)
+with open(output_file, "w") as text_file:
+    text_file.write (f"Financial Analysis")
+    text_file.write (f"-------------------------------")
+    text_file.write (f"Total Months: ", row_count)
+    #print (f"Total: {}".format(net))
+    #print (f"Average Change: ")
+    #print (f"Greatest Increase in Profits: ")
+    #print (f"Greatest Decrease in Profits: ")    
