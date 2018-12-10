55f3212e7d44e1ecca39110802bbbc00812d404a
The Hoff <11578521+xjarodx@users.noreply.github.com>
2018-12-09 22:36:51 -0500

    still stuck on getting sum of second column.

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,6 +3,14 @@ import csv
 
 budget_csv = os.path.join("../PyBank", "budget_data.csv")
 
+#def average(numbers):
+#    length = len(csvreader)
+#    total = 0.0
+#    for number[1] in numbers:
+#        total += number
+#    return total / length
+
+
 with open(budget_csv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     #skip header row
@@ -10,24 +18,24 @@ with open(budget_csv, newline="") as csvfile:
     
     # Gets number of rows in csv
     data=list(csvreader)
-    
+
     row_count = len(data)
-
-    #Sets initial value of the net sum
-    net = 0
-
-    #gets values from rows
+    
+    #B = {rows[0]:rows[1] for rows in csvreader}
+    #print (sum(B.values()))
+    totals = []
     for row in csvreader:
-        net += float(row[1])
+        values = row[1]
+        totals.append(values)
 
-
+    print (totals)
 
 
 
 print ("Financial Analysis")
 print ("-------------------------------")
 print ("Total Months: ", row_count)
-print ("Total: {}".format(net))
+print ("Total: ", sum(totals))
 print ("Average Change: ")
 print ("Greatest Increase in Profits: ")
 print ("Greatest Decrease in Profits: ")
@@ -38,9 +46,9 @@ output_file = os.path.join("Analysis.txt")
 
 #  Open the output file
 with open(output_file, "w") as text_file:
-    text_file.write (f"Financial Analysis")
-    text_file.write (f"-------------------------------")
-    text_file.write (f"Total Months: ", row_count)
+    text_file.write (f"Financial Analysis\n")
+    text_file.write (f"-------------------------------\n")
+    text_file.write (f"Total Months: " + str(row_count) + "\n")
     #print (f"Total: {}".format(net))
     #print (f"Average Change: ")
     #print (f"Greatest Increase in Profits: ")
